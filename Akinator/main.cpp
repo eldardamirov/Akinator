ba741f46120dbe70bd82b7842364a2d1d2f3124f
eldardamirov <eldardamirov75@mail.ru>
2018-07-01 11:39:28 +0300

    Initial Commit

---
+++ Akinator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Akinator
+//
+//  Created by Эльдар Дамиров on 01.07.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
